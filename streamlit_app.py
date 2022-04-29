b07a3dc4e59575b5f1693be1fc2c7b1dabeb8301
santiave <98056870+santiave@users.noreply.github.com>
2022-04-29 15:10:14 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -67,8 +67,52 @@ if __name__ == "__main__":
 
     st.balloons()
     if st.button('Start Selenium run'):
-        st.info('Selenium is running, please wait...')
-        result = run_selenium()
-        st.info(f'Result -> {result}')
-        st.info('Successful finished. Selenium log file is shown below...')
-        show_selenium_log()
+        with st.spinner(text="Actualizando Dasboard..."):
+
+
+
+            driver = webdriver.Chrome()
+
+            driver.implicitly_wait(10) # seconds
+
+
+            driver.get("https://app.powerbi.com/groups/me/datasets/d6d2d0f2-f1c7-429f-86e5-f594d17a3c3e/details")
+            user = 'savellaneda@promotive.com.ar'
+            password = 'Prombi23'
+
+
+            username = driver.find_element('id', value='email')
+
+            username.send_keys(user)
+            driver.find_element('id', "submitBtn").click()
+
+            driver.find_element('name', 'passwd').send_keys(password)
+
+            buttton = driver.find_element('id', 'idSIButton9')
+
+            buttton.click()
+
+            buttton = driver.find_element('id', 'idSIButton9')
+
+            buttton.click()
+
+            actualizacion = driver.find_element(By.XPATH, '//*[@id="content"]/dataset-related-reports-container/section/section/dataset-information/section/dataset-details-card/mat-card/mat-card-content/section/dataset-details-field[2]/mat-card-subtitle/section[2]/dataset-icon-container-modern/span[1]')
+
+            actual = actualizacion.text
+
+            st.write(f'Última actualización: {actual}')
+
+            buttton = driver.find_element(By.XPATH ,'//*[@id="content"]/dataset-related-reports-container/dataset-action-bar/action-bar/action-button[2]/button')
+            buttton.click()
+
+
+
+            actualizar_bot = driver.find_element(By.XPATH ,'//*[@id="mat-menu-panel-8"]/div/button[1]')
+
+            actualizar_bot.click()
+
+            actualizacion = driver.find_element(By.XPATH, '//*[@id="content"]/dataset-related-reports-container/section/section/dataset-information/section/dataset-details-card/mat-card/mat-card-content/section/dataset-details-field[2]/mat-card-subtitle/section[2]/dataset-icon-container-modern/span[1]')
+
+            el = WebDriverWait(driver, timeout=500).until(lambda d: actual != actualizacion.text)
+
+        st.success('Actualización extitosa!')
