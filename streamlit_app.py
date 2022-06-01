951491096c08ac33cebc6c7f38ef8cc69f7bceb7
santiave <98056870+santiave@users.noreply.github.com>
2022-06-01 19:15:53 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,47 +22,18 @@ options.add_argument("--disable-features=VizDisplayCompositor")
 
 
 driver = webdriver.Chrome(options=options, service_log_path='selenium.log')
-
-driver.implicitly_wait(10) # seconds
-
-
-driver.get("https://app.powerbi.com/groups/me/datasets/d6d2d0f2-f1c7-429f-86e5-f594d17a3c3e/details")
-user = 'savellaneda@promotive.com.ar'
-password = 'Prombi23'
-
-
-username = driver.find_element('id', value='email')
-
-username.send_keys(user)
-driver.find_element('id', "submitBtn").click()
-
-driver.find_element('name', 'passwd').send_keys(password)
-
-buttton = driver.find_element('id', 'idSIButton9')
-
-buttton.click()
-
-buttton = driver.find_element('id', 'idSIButton9')
-
-buttton.click()
-
-actualizacion = driver.find_element(By.XPATH, '//*[@id="content"]/dataset-related-reports-container/section/section/dataset-information/section/dataset-details-card/mat-card/mat-card-content/section/dataset-details-field[2]/mat-card-subtitle/section[2]/dataset-icon-container-modern/span[1]')
-
-actual = actualizacion.text
-
-st.write(f'Última actualización: {actual}')
-
-buttton = driver.find_element(By.XPATH ,'//*[@id="content"]/dataset-related-reports-container/dataset-action-bar/action-bar/action-button[2]/button')
-buttton.click()
+driver.implicitly_wait(3)
+driver.get('https://cuenta.docturno.com/ingresar')
+mail= driver.find_element(By.XPATH,'//*[@id="email"]')
+mail.send_keys('Info@athentun.org')
+mail.submit()
+time.sleep(1)  
 
 
+passw=driver.find_element(By.XPATH, '//*[@id="password"]')
+time.sleep(1)  
+passw.send_keys('44528765')
 
-actualizar_bot = driver.find_element(By.XPATH ,'//*[@id="mat-menu-panel-8"]/div/button[1]')
-
-actualizar_bot.click()
-
-actualizacion = driver.find_element(By.XPATH, '//*[@id="content"]/dataset-related-reports-container/section/section/dataset-information/section/dataset-details-card/mat-card/mat-card-content/section/dataset-details-field[2]/mat-card-subtitle/section[2]/dataset-icon-container-modern/span[1]')
-
-el = WebDriverWait(driver, timeout=500).until(lambda d: actual != actualizacion.text)
-
-st.success('Actualización extitosa!')
+boton = driver.find_element(By.CLASS_NAME, 'signin-password-action')
+boton.click()
+st.write(driver.driver.get_screenshot_as_png())
